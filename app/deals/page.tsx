4aecddcfc4cf4c7096f35040d078c8fabab80a2f
ArlatPS <przemekx10@gmail.com>
2023-03-11 19:03:03 +0100

    state reducer completed

--- app/deals/page.tsx
+++ app/deals/page.tsx
@@ -3,10 +3,11 @@ import { useCallback, useEffect, useState, useRef, useReducer } from "react";
 import { DealsListGame } from "@/globalTypes";
 import AllDealsList from "./allDealsList";
 
-type ControlsState = {
+type State = {
   page: number;
   hasUpdated: boolean;
   maxPages: number;
+  deals: DealsListGame[];
 };
 
 type SetMaxPages = {
@@ -14,18 +15,28 @@ type SetMaxPages = {
   payload: number;
 };
 
-type ControlsActionsWithPayload = SetMaxPages;
+type SetDeals = {
+  type: "setDeals";
+  payload: DealsListGame[];
+};
 
-type ControlsActionsWithoutPayload = {
+type StateActionsWithPayload = SetMaxPages | SetDeals;
+
+type StateActionsWithoutPayload = {
   type: "nextPage" | "prevPage" | "setHasUpdatedTrue" | "setHasUpdatedFalse";
 };
 
-type ControlsActions =
-  | ControlsActionsWithoutPayload
-  | ControlsActionsWithPayload;
+type StateActions = StateActionsWithoutPayload | StateActionsWithPayload;
 
-function controlsReducer(state: ControlsState, action: ControlsActions) {
+function stateReducer(state: State, action: StateActions) {
   switch (action.type) {
+    case "setDeals":
+      if (action.payload) {
+        return {
+          ...state,
+          deals: action.payload,
+        };
+      }
     case "nextPage":
       if (state.page < state.maxPages) {
         return {
@@ -70,64 +81,52 @@ function controlsReducer(state: ControlsState, action: ControlsActions) {
   }
 }
 
-const initialStateOfControls: ControlsState = {
+const initialState: State = {
   page: 0,
-  hasUpdated: true,
+  hasUpdated: false,
   maxPages: 0,
+  deals: [],
 };
 
 export default function Deals() {
-  const [deals, setDeals] = useState<DealsListGame[]>([]);
-  // ref for keeping track of whether user is waiting for next page to load
-  // const hasUpdated = useRef(true);
-
-  // controls reducer
-  const [controls, dispatchControls] = useReducer(
-    controlsReducer,
-    initialStateOfControls
-  );
+  // state reducer
+  const [state, dispatchState] = useReducer(stateReducer, initialState);
 
   useEffect(() => {
     async function fetchData() {
       // revalidate all deals every 10 minutes
       const response = await fetch(
-        `https://www.cheapshark.com/api/1.0/deals?pageNumber=${controls.page}&pageSize=20`,
+        `https://www.cheapshark.com/api/1.0/deals?pageNumber=${state.page}&pageSize=20`,
         { next: { revalidate: 10 * 60 } }
       );
       const res = await response.json();
       // get max-pages from header
       if (response.headers.get("x-total-page-count")) {
-        dispatchControls({
+        dispatchState({
           type: "setMaxPages",
           payload: Number(response.headers.get("x-total-page-count")),
         });
       }
       // updated is true
-      dispatchControls({ type: "setHasUpdatedTrue" });
-      setDeals(res);
+      dispatchState({ type: "setHasUpdatedTrue" });
+      dispatchState({ type: "setDeals", payload: res });
     }
     fetchData();
-  }, [controls.page]);
-
-  if (!deals || !deals?.length) {
-    return <h1>loading</h1>;
-  }
+  }, [state.page]);
 
   return (
     <div>
       <h2>All Deals</h2>
-      <AllDealsList deals={deals} hasUpdated={controls.hasUpdated} />
+      <AllDealsList deals={state.deals} hasUpdated={state.hasUpdated} />
       <h5>
-        <button onClick={() => dispatchControls({ type: "prevPage" })}>
+        <button onClick={() => dispatchState({ type: "prevPage" })}>
           Prev
         </button>
-        <button onClick={() => dispatchControls({ type: "nextPage" })}>
+        <button onClick={() => dispatchState({ type: "nextPage" })}>
           Next
         </button>
-        Page {controls.page + 1}/{controls.maxPages}
+        Page {state.page + 1}/{state.maxPages}
       </h5>
-      {/* indicator for the user that fetching is being done */}
-      {controls.hasUpdated ? null : <h4>🌀</h4>}
     </div>
   );
 }
