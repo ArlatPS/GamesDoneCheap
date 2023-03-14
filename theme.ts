32031d7f4eaaa53491f718d4644bb3e896a9b5d4
ArlatPS <przemekx10@gmail.com>
2023-03-14 22:50:10 +0100

    theme

--- theme.ts
+++ theme.ts
@@ -1,10 +1,10 @@
 export const theme = {
   colors: {
-    blue100: "#94C1DD",
-    blue200: "#4FA2D6",
-    blue300: "#3C7CA3",
-    blue400: "#3A4C57",
-    blue500: "#204257",
+    night: "#131112",
+    red: "#FF0303",
+    grey: "#60656F",
+    blue: "#279AF1",
+    white: "#F7F7FF",
   },
   xl: "1200px",
   md: "768px",
