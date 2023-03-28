e63ccad4547949f71fcef04b4d2df15bd84c0552
ArlatPS <przemekx10@gmail.com>
2023-03-29 00:39:52 +0200

    shark fetching for user games

--- components/auth/userDeals.tsx
+++ components/auth/userDeals.tsx
@@ -1,3 +1,4 @@
+import { GameFromShark } from "@/globalTypes";
 import { getUserGamesIds } from "@/lib/userLib/getUserGamesIds";
 import {
   currentUser,
@@ -10,13 +11,24 @@ import { use } from "react";
 export default async function UserDeals() {
   const user = await currentUser();
   let userGamesIds: string[] = [];
+  let games: GameFromShark[] = [];
   if (user !== null) {
     userGamesIds = await getUserGamesIds(user.id);
+    // if userGameIds not empty then fetch first 25 (max for one fetch to this API) from Shark
+    if (userGamesIds.length > 0) {
+      const response = await fetch(
+        `https://www.cheapshark.com/api/1.0/games?ids=${userGamesIds.join(
+          ","
+        )}`,
+        { next: { revalidate: 10 } }
+      );
+      games = await response.json();
+    }
   }
   return (
     <SignedIn>
       <h2>Deals for {user?.username}</h2>
-      <h3>{userGamesIds[0]}</h3>
+      <h3>{games[128]?.info?.title}</h3>
     </SignedIn>
   );
 }
