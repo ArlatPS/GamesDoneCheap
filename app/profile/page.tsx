3a0a5e6565c849ad1f2ef41b4d0b48762832743d
ArlatPS <przemekx10@gmail.com>
2023-03-30 18:35:09 +0200

    user profile styled and responsive

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,3 +1,4 @@
+import { ProfileMainStyled } from "@/style/profile/profileStyled";
 import {
   currentUser,
   SignedIn,
@@ -5,19 +6,35 @@ import {
   UserProfile,
   SignIn,
 } from "@clerk/nextjs/app-beta";
+import { dark } from "@clerk/themes";
 
 export default async function UserPage() {
   const user = await currentUser();
 
   return (
-    <section>
+    <ProfileMainStyled>
       <SignedIn>
-        <h2>{user?.id}</h2>
-        <UserProfile />
+        <section className="signedInSection">
+          <div className="userGames">
+            <h2>{user?.id}</h2>
+          </div>
+          <div className="userProfile">
+            <UserProfile
+              appearance={{
+                baseTheme: dark,
+                elements: {
+                  navbar: { flexBasis: "4rem" },
+                  card: { width: "100%" },
+                  rootBox: { width: "100%" },
+                },
+              }}
+            />
+          </div>
+        </section>
       </SignedIn>
       <SignedOut>
         <SignIn />
       </SignedOut>
-    </section>
+    </ProfileMainStyled>
   );
 }
---
+++ style/profile/profileStyled.ts
@@ -0,0 +1,27 @@
+"use client";
+import styled from "styled-components";
+import { theme } from "@/theme";
+
+export const ProfileMainStyled = styled.main`
+  margin: 0 10%;
+  @media (min-width: ${theme.xxl}) {
+    margin: 0 20%;
+  }
+  @media (max-width: ${theme.md}) {
+    margin: 0 5%;
+  }
+  @media (max-width: ${theme.sm}) {
+    margin: 0 5px;
+  }
+  .signedInSection {
+    @media (min-width: ${theme.md}) {
+      display: flex;
+      .userGames {
+        width: 50%;
+      }
+      .userProfile {
+        width: 50%;
+      }
+    }
+  }
+`;
